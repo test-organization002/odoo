1f9e49127654e315d1f63d448516fca1ec9f30b9
erhan-hash <erhan.cekin@ode.al>
2025-08-28 22:19:03 +0300

    feat(menu): add basic menu structure for Odoo 18 compatibility testing

--- terminal_management/__manifest__.py
+++ terminal_management/__manifest__.py
@@ -19,12 +19,12 @@
     'web_icon': 'terminal_management/static/description/icon.svg',
     'data': [
         'security/ir.model.access.csv',
+        'views/menu_views.xml',  # Basic menu structure
         # Temporarily disabled for Odoo 18 compatibility testing
         # 'views/terminal_views.xml',
         # 'views/banka_durum_views.xml',
         # 'views/pos_durum_views.xml',
         # 'views/siparis_durum_views.xml',
-        # 'views/menu_views.xml',
         # 'data/demo_data.xml',
     ],
     'assets': {
